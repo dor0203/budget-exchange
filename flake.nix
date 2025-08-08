c09952e6a84439626e0d9f0d95475b0985b263bf
dor cohen <dorcohen0203@gmail.com>
2025-08-08 12:13:10 +0300

    fixed postgresql

--- flake.nix
+++ flake.nix
@@ -11,14 +11,18 @@
         initializeDatabase = ''
           export PGDATA="$(pwd)/.pgdata"
           export PGHOST="$PGDATA"
+          flag=false
+
           if [ ! -d "$PGDATA" ]; then
             initdb --pgdata=$PGDATA -c "unix_socket_directories=$PGDATA" > /dev/null
+            echo "unix_socket_directories = '/tmp'" >> $PGDATA/postgresql.conf
           fi
 
           alias psql="psql --host=$PGDATA"
           pg_ctl --pgdata=$PGDATA -l "$PGDATA/logfile" start
           trap "pg_ctl -D \"$PGDATA\" stop" EXIT
         '';
+
 
       in with pkgs; {
         devShells.default = mkShell {
@@ -34,7 +38,6 @@
 
           shellHook = ''
             yarn install
-            "$EDITOR"
             ${initializeDatabase}
           '';
         };
